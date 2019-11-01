91356c0539242d5d73401652a90c43b94632396d
Remi LeBlanc <remwileblanc@gmail.com>
2019-11-01 12:55:14 -0400

    Connect to project and print out out workflow and subject sets

--- script.py
+++ script.py
@@ -1,3 +1,11 @@
 from panoptes_client import Panoptes, Project
 
-Panoptes.connect(username='example', password='example')
+Panoptes.connect(username='', password='')
+
+project = Project.find("6307")
+print(project.display_name)
+
+for workflow in project.links.workflows:
+    print(workflow.display_name)
+
+print(project.links.subject_sets)
