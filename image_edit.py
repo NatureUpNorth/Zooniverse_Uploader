07a18a99ca60a6f03e5fface209dd5e4337c7702
Remi LeBlanc <remwileblanc@gmail.com>
2020-02-23 23:44:37 -0500

    completed script. Added error files and more error logs, manifest still not working yet

--- image_edit.py
+++ image_edit.py
@@ -2,13 +2,16 @@ from PIL import Image
 import os
 import glob
 import time
+import csv
+import shutil
 from panoptes_client import Panoptes, Project, SubjectSet, Subject
 
-
-file_path = "/Users/remileblanc/Dropbox/College/Nature Up North/Opossum/test.JPG"
 dir_path = "/Users/remileblanc/Desktop/test_pics/*"
 out_path = "/Users/remileblanc/Desktop/ResizedImages"
 path = "/Users/remileblanc/Desktop/ResizedImages/*"
+logfile = "/Users/remileblanc/Desktop/log.txt"
+errorfiles = "/Users/remileblanc/Desktop/ErrorFiles/"
+completed_images = "/Users/remileblanc/Desktop/completed_images"
 
 target_size = 1000000
 
@@ -57,17 +60,25 @@ for image in images:
             os.rename(image, out_path+'/'+os.path.basename(os.path.normpath(image)))
 
         else:
-            f = open("/Users/remileblanc/Desktop/log.txt", "a")
+            f = open(logfile, "a")
             t = time.localtime()
-
-            f.write('\nFailure whilst processing "' + image + '": ' + str(e)+ " " + time.strftime("%D:%H:%M:%S",t))
+            f.write('\nFailure whilst processing "' + image + '": ' + str(e)+ " " + time.strftime("%D:%H:%M:%S",t)+'\n\n')
             f.close()
+            # move error files into seperate folder
+            os.rename(image, errorfiles + os.path.basename(os.path.normpath(image)))
+
 # delete the tmp file after the images have been resized
 
-Panoptes.connect(username='natureupnorth@gmail.com', password='NatureRocks4')
+try:
+    Panoptes.connect(username='', password='')
+    project = Project.find("6307")
+except Exception as e:
+    f = open(logfile, "a")
+    t = time.localtime()
 
-project = Project.find("6307")
-print(project.display_name)
+    f.write('Unable to connect to Zooniverse: '+time.strftime("%D:%H:%M:%S",t)+'\n')
+    f.close()
+
 
 subject_set = SubjectSet()
 s = Subject()
@@ -79,25 +90,52 @@ images = glob.glob(path)
 new_subjects = []
 
 for img in images:
-    print(img)
-    s = Subject()
-    s.links.project = project
-    # if os.path.splitext(img)[1] == ".csv":
-    #     s.metadata.update(img)
-    # else:
-    s.add_location(img)
-    s.save()
-    new_subjects.append(s)
-subject_set.save()
-subject_set.add(new_subjects)
+    try:
+        s = Subject()
+        s.links.project = project
+        # manifest file
+        if os.path.splitext(img)[1] == ".csv":
+            # move csv to complete images folder
+            shutil.copy(f, completed_images)
+            # make dict out of csv file for upload
+            manifest = csv.DictReader(open(img))
+            s.metadata.update(manifest)
+        else:
+            # upload image to subject
+            s.add_location(img)
+            s.save()
+            new_subjects.append(s)
+    except Exception as e:
+        f = open(logfile, "a")
+        t = time.localtime()
+        # move error files into seperate folder
+        os.rename(img, errorfiles + os.path.basename(os.path.normpath(img)))
+        f.write('Unable to upload ' + img + ': ' + str(e) + ' '+time.strftime("%D:%H:%M:%S", t)+'\n\n')
+        f.close()
 
-# once upload to zooniverse is complete, delete resized images
-for img in images:
-    os.remove(img)
+try:
+    # add subjects to subject set
+    subject_set.save()
+    subject_set.add(new_subjects)
+except Exception as e:
+    f = open(logfile, "a")
+    t = time.localtime()
+    f.write('Unable to save subject set: ' + str(e) + ' '+time.strftime("%D:%H:%M:%S", t)+'\n\n')
+    f.close()
+
 
 # move uploaded images out of original folder to archive
 og_images = glob.glob(dir_path)
-completed_images = "/Users/remileblanc/Desktop/completed_images/"
 for img in og_images:
-    os.rename(img, completed_images+os.path.basename(os.path.normpath(img)))
+    os.rename(img, completed_images+'/'+os.path.basename(os.path.normpath(img)))
+
+
+# once upload to zooniverse is complete, delete resized images
+for img in images:
+    try:
+        os.remove(img)
+    except:
+        # file was already moved out of directory, likely due to it causing an error
+        pass
+
 
